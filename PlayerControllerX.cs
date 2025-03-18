b6ceb5b255666c72c732668dd266dfcb7109f6ab
Colm Nolan_B00152001 <B00152001@mytudublin.ie>
2025-03-18 13:53:55 +0000

    Update PlayerControllerX.cs

--- PlayerControllerX.cs
+++ PlayerControllerX.cs
@@ -1,14 +1,1 @@
-﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    public float speed = 5f;
-    private Vector3 playerVelocity;
-
-    void FixedUpdate()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        playerVelocity = new Vector3(horizontalInput * speed, 0f, 0f);
-        transform.Translate(playerVelocity * Time.deltaTime, Space.Self);
-    }
-}
